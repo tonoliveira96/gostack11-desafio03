3231c3869891974bc3fe1d1ca2e8859df7b91380
Everton <ton.oliveira@sempre.facig.edu.br>
2020-04-10 11:01:28 -0300

    Adicionado estilos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import api from  './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(()=>{
+    api.get('/repositories').then(response=>{
+      setRepositories(response.data)
+    })
+  }, [])
+
   async function handleAddRepository() {
     // TODO
+    //setRepositories([...repositories, 'Teste'])
+    api.post('/repositories',{
+      
+    })
   }
 
   async function handleRemoveRepository(id) {
@@ -14,13 +27,17 @@ function App() {
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+       {
+         repositories.map(repo=>
+          <li key={repo.id}> 
+          {repo.title}
 
           <button onClick={() => handleRemoveRepository(1)}>
             Remover
           </button>
         </li>
+        )
+       }                
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
